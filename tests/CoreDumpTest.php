2e39e3afad4154e9f3192bbf048f76720ebf5b2b
Michael Hall <hall@michaelhall.se>
2022-09-20 17:43:45 +0200

    Set correct visibility for test methods

--- tests/CoreDumpTest.php
+++ tests/CoreDumpTest.php
@@ -182,7 +182,7 @@ class CoreDumpTest extends TestCase
     /**
      * Set up.
      */
-    public function setUp(): void
+    protected function setUp(): void
     {
         parent::setUp();
 
@@ -201,7 +201,7 @@ class CoreDumpTest extends TestCase
     /**
      * Tear down.
      */
-    public function tearDown(): void
+    protected function tearDown(): void
     {
         parent::tearDown();
 
